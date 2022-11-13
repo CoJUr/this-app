15d33e3262e4b3a0dc86cfbef6f26ad1b560d85b
“Cory <coryurton@gmail.com>
2022-11-12 21:04:44 -0600

    feat: Os taking Over

--- src/index.js
+++ src/index.js
@@ -17,12 +17,19 @@ class Board extends React.Component {
         super(props);
         this.state = {
             squares: Array(9).fill(null),
+            xIsNext: true,
         };
     }
 
+
+
     handleClick(i) {
         const squares = this.state.squares.slice();
-        squares[i] = 'X';
+        squares[i] = this.state.xIsNext ? 'X' : 'O';
+        this.setState({
+            squares: squares,
+            xIsNext: !this.state.xIsNext,
+        });
         this.setState({squares: squares});
     }
 
@@ -36,7 +43,7 @@ class Board extends React.Component {
     }
 
     render() {
-        const status = 'Next player: X';
+        const status = 'Next player: ' + (this.state.xIsNext ? 'X' : 'O');
 
         return (
             <div>
